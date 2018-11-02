68b1f0006bc313b8220271d36a64762e422210eb
doanhdoanh <44568768+nguyenkhadoanh@users.noreply.github.com>
2018-11-02 14:05:12 +0700

    omg

--- helloWorld.py
+++ helloWorld.py
@@ -1,5 +1,6 @@
 for i in range(0, 10): print("GCI is great")
 print("What's your name ?")
 name = input()
-name=name[::-1]
 print("Hello "+name+", please to meet you!")
+name=name[::-1]
+print(name)
